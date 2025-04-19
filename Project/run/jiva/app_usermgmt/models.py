9ce854fdecbf1ba8beb341df0539b4cebdba334f
Arun R G <arun.agility@gmail.com>
2025-04-19 21:21:31 +0530

    check

--- Project/run/jiva/app_usermgmt/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# User mgmt uses the default user model provided by Django, so no custom user model is defined here.
-# The default user model is defined in django.contrib.auth.models.User.
